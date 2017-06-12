8457aa89e4322fc1af70bf6f9f902bb4d826c6a0
Kate Latypova <lakate@yandex-team.ru>
2017-06-12 21:06:16 +0500

    календарь и структура

--- bundles/companies/companies.js
+++ bundles/companies/companies.js
@@ -3,15 +3,23 @@ require('./companies.css')
 window.onload = function() {
     var buttons = document.getElementsByClassName('resume'),
         resume = document.getElementsByClassName('sended-resume'),
-        thanks = document.getElementsByClassName('thanks');
+        thanks = document.getElementsByClassName('thanks'),
+        structureLink = document.getElementsByClassName('structure-link'),
+        worksWrapper = document.getElementsByClassName('work-container'),
+        structure = document.getElementsByClassName('structure');
 
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(e) {
             resume[0].style.display = "block";
-        }, false);
+        });
     }
 
     thanks[0].addEventListener('click', function() {
         resume[0].style.display = "none";
+    });
+
+    structureLink[0].addEventListener('click', function() {
+        worksWrapper[0].style.display = "none";
+        structure[0].style.display = "block";
     });
   };
--- bundles/profile/profile.js
+++ bundles/profile/profile.js
@@ -1,1 +1,36 @@
 require('./profile.css')
+
+window.onload = function() {
+    var close = document.getElementsByClassName('close-button'),
+        calendar = document.getElementsByClassName('calendar'),
+        bigCalendar = document.getElementsByClassName('calendar-big'),
+        bigCalendarImg = document.getElementsByClassName('calendar-big__img'),
+        bigCalendarText = document.getElementsByClassName('calendar-big__text'),
+        bigCalendarSubmit = document.getElementsByClassName('invisible-submit'),
+        bigCalendarThanks = document.getElementsByClassName('calendar-thanks');
+
+    calendar[0].addEventListener('click', function(e) {
+        bigCalendar[0].style.display = 'block';
+        bigCalendarImg[0].style.display = 'block';
+    });
+
+    close[0].addEventListener('click', function() {
+        bigCalendar[0].style.display = 'none';
+        bigCalendarImg[0].style.display = 'none';
+        bigCalendarText[0].style.display = 'none';
+    });
+
+    bigCalendarImg[0].addEventListener('click', function(e) {
+        bigCalendarImg[0].style.display = 'none';
+        bigCalendarText[0].style.display = 'block';
+        bigCalendar[0].style.width = '300px';
+        bigCalendar[0].style.height = '200px';
+        bigCalendar[0].style.top = '200px';
+        bigCalendar[0].style['margin-left'] = '-150px';
+    });
+
+    bigCalendarSubmit[0].addEventListener('click', function(e) {
+        bigCalendarText[0].style.display = 'none';
+        bigCalendarThanks[0].style.display = 'block';
+    });
+};
