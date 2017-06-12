b0cc8ac4e03e421c14f9abda255b8f0f2aaffe7a
Kate Latypova <lakate@yandex-team.ru>
2017-06-12 17:53:52 +0500

    ваше резюме отправлено

--- bundles/companies/companies.js
+++ bundles/companies/companies.js
@@ -1,1 +1,17 @@
 require('./companies.css')
+
+window.onload = function() {
+    var buttons = document.getElementsByClassName('resume'),
+        resume = document.getElementsByClassName('sended-resume'),
+        thanks = document.getElementsByClassName('thanks');
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(e) {
+            resume[0].style.display = "block";
+        }, false);
+    }
+
+    thanks[0].addEventListener('click', function() {
+        resume[0].style.display = "none";
+    });
+  };
